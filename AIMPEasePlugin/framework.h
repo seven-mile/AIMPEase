acd966f2cce383a5ed480aeeeedea53219162c8d
7mile <i@7li.moe>
2021-08-21 02:15:07 +0800

    dev: init project and install cpp/winrt & wil

---
+++ AIMPEasePlugin/framework.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+// WIL & cpp/winrt
+#include <wil/cppwinrt.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+namespace winrt {
+	using namespace Windows::Foundation;
+}
+
+// AIMP SDK
+#include "aimp_sdk/apiCore.h"
